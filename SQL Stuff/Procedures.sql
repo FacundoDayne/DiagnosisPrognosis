9ef8ea4f1e4392e5765f2997f721de592aa2aae2
FacundoDayne <facundo.088947@globalcity.sti.edu.ph>
2022-11-29 22:14:26 +0800

    completed the procedures in the database and moving to add the database features to the application itself. good luck to me happy face

--- SQL Stuff/Procedures.sql
+++ SQL Stuff/Procedures.sql
@@ -29,12 +29,6 @@ as
 	@patient_medicalneeds
 	);
 
--- Procedure to delete a Patient // Is broken atm
-create proc DeletePatient
-@patient_name varchar(100)
-as
-	delete from PatientTable where patient_name = @patient_name;
-
 -- Procedure to get illness - symptom relations via symptom_name
 create proc GetIllnessSymptomViaSymptomName
 @symptom_name varchar(50)
@@ -62,7 +56,6 @@ as
 	-- example: where b.illness_name = 'Common Cold';
 
 -- Procedure to get number of symptoms each illness have
-
 create proc GetIllnessNumberOfSymptoms
 @illness_name varchar(50)
 as
@@ -75,32 +68,82 @@ as
 	-- example: where a.illness_name = 'Common Cold'
 	group by illness_name;
 
--- Procedure to get Patient's Data DOESNT WORK ATM
+-- Procedure to get Patient's Data via Name
 create proc GetPatientViaName
+@patient_name varchar(50)
+as
+	select
+	a.patient_studentnumber as 'Student Number',
+	a.patient_lastname + ', ' + a.patient_firstname + ' ' + a.patient_middlename as 'Full Name',
+	a.patient_age as 'Age',
+	b.sex_name as 'Sex',
+	c.course_name as 'Course',
+	a.patient_contactnumber as 'Contact Number',
+	a.patient_birthdate as 'Birthday',
+	case 
+		when cast(a.patient_allergies as int) = 1 then 'Yes'
+		else 'No'
+	end as 'Allergy Concerns',
+	case 
+		when cast(a.patient_consultation as int) = 1 then 'Yes'
+		else 'No'
+	end as 'Consultation Concerns',
+	case 
+		when cast(a.patient_checkup as int) = 1 then 'Yes'
+		else 'No'
+	end as 'Check up',
+	case 
+		when cast(a.patient_medicalneeds as int) = 1 then 'Yes'
+		else 'No'
+	end as 'Medical Needs'
+	from PatientTable a
+	inner join SexTable b on a.patient_sex_id = b.sex_id
+	inner join CourseTable c on a.patient_course_id = c.course_id
+	where (a.patient_lastname + ', ' + a.patient_firstname + ' ' + a.patient_middlename) = @patient_name;
 
-declare
-@fullname as varchar(100),
-@allergies as varchar(3),
-@consultation as varchar(3),
-@checkup as varchar(3),
-@medicalneeds as varchar(3)
+-- Procedure to get Patient's Data via Student Number
+create proc GetPatientViaStudentNumber
+@patient_studentnumber int
+as
+	select
+	a.patient_studentnumber as 'Student Number',
+	a.patient_lastname + ', ' + a.patient_firstname + ' ' + a.patient_middlename as 'Full Name',
+	a.patient_age as 'Age',
+	b.sex_name as 'Sex',
+	c.course_name as 'Course',
+	a.patient_contactnumber as 'Contact Number',
+	a.patient_birthdate as 'Birthday',
+	case 
+		when cast(a.patient_allergies as int) = 1 then 'Yes'
+		else 'No'
+	end as 'Allergy Concerns',
+	case 
+		when cast(a.patient_consultation as int) = 1 then 'Yes'
+		else 'No'
+	end as 'Consultation Concerns',
+	case 
+		when cast(a.patient_checkup as int) = 1 then 'Yes'
+		else 'No'
+	end as 'Check up',
+	case 
+		when cast(a.patient_medicalneeds as int) = 1 then 'Yes'
+		else 'No'
+	end as 'Medical Needs'
+	from PatientTable a
+	inner join SexTable b on a.patient_sex_id = b.sex_id
+	inner join CourseTable c on a.patient_course_id = c.course_id
+	where a.patient_studentnumber = @patient_studentnumber;
 
-if (select cast(patient_allergies as int)from PatientTable) = 1 set @allergies = 'Yes' else set @allergies = 'No'
-if (select cast(patient_consultation as int)from PatientTable) = 1 set @consultation = 'Yes' else set	@consultation = 'No'
-if (select cast(patient_checkup as int)from PatientTable) = 1 set @checkup = 'Yes' else set @checkup =	'No'
-if (select cast(patient_medicalneeds as int)from PatientTable) = 1 set @medicalneeds = 'Yes' else set	@medicalneeds = 'No'	
-select 
-a.patient_lastname as 'Full Name',
-a.patient_age as 'Age',
-b.sex_name as 'Sex',
-c.course_name as 'Course',
-a.patient_contactnumber as 'Contact Number',
-a.patient_birthdate as 'Birthday',
-@allergies as 'Allergy Concerns',
-@consultation as 'Consultation Concerns',
-@checkup as 'Check up',
-@medicalneeds as 'Medical Needs'
-from PatientTable a
-inner join SexTable b on a.patient_sex_id = b.sex_id
-inner join CourseTable c on a.patient_course_id = c.course_id
---where 'Facundo, Dayne Santos'
+-- Procedure to delete a patient via Name
+create proc DeletePatientViaName
+@patient_name varchar(50)
+as
+	delete from PatientTable 
+	where (patient_lastname + ', ' + patient_firstname + ' ' + patient_middlename) = @patient_name;
+
+-- Procedure to delete a patient via Student Number
+create proc DeletePatientViaStudentNumber
+@patient_studentnumber int
+as
+	delete from PatientTable 
+	where patient_studentnumber = @patient_studentnumber;
